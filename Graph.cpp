911d8e8178555da1fe483cadfb7db2d1dfe7f43c
Mayank Srivastav <mayanksri18@yahoo.in>
2015-07-29 15:56:34 +0530

    Updated Graph Algorithms
    Updated with BFS algorithms

--- Graph.cpp
+++ Graph.cpp
@@ -1,21 +1,28 @@
-// Graph algorithms
-
-#include <iostream>
+// Algorithms on Graph data structure
+	
 #include <algorithm>
+#include <cmath>
+#include <iostream>
 #include <vector>
-#include <stdio.h>
+#include <stack>
+#include <queue>
+#include <map>
 using namespace std;
+
+#pragma region Graph DS
+typedef pair<int, int> ii; 
+typedef vector<ii> vii;
+typedef vector<vii> AdjList;
+#pragma endregion
+
+#define TRvii(c, it) for (vii::iterator it = c.begin(); it != c.end(); it++)
+#define REP(i, a, b) for (int i = int(a); i <= int(b); i++)
 
 const int V = 8;
 const int DFS_BLACK = 1;
 const int DFS_WHITE = -1;
 
-#pragma region Graph DS
-typedef pair<int, int> ii;
-typedef vector<ii> vii;
-typedef vector<vii> AdjList;
-#pragma endregion
-
+// Global variables and objects
 int dfs_num[V];
 int dfs_low[V];
 int dfs_parent[V];
@@ -24,27 +31,70 @@ int dfs_root = 0;
 int rootChildren = 0;
 int dfsNumberCounter = 0;
 AdjList adjlist(8);
+stack<int> topologicalSort;
+bool isCyclic = false;
+queue<int> q;
+map<int, int> dist;	
 
-#define TRVii(c, it) for (vii::iterator it = c.begin(); it != c.end(); ++it)
-#define Rep(i, a, b) for (int i = int(a); i <= int(b); ++i)
+// Normal DFS, Directed graph Cycle find
+// Undirected Graph Connected component 
+// Directed graph strongly connected component
+bool dfs(int u) 
+{ // DFS for normal usage
+	printf(" %d", u); 
+	dfs_num[u] = DFS_BLACK; // this vertex is visited, mark it
+	//TRvii (adjlist[u], v) // try all neighbors v of vertex u
+	for (vii::iterator it = adjlist[u].begin(); it != adjlist[u].end(); it++) {
+		if (dfs_num[it->first] == DFS_WHITE) // avoid cycle
+			dfs(it->first); // v is a (neighbor, weight) pair
+		else isCyclic = true;
+	}
+	//topologicalSort.push_back(u);
+	//topologicalSort.push(u);
+	return isCyclic;
+}
 
-// DFS traversal, uses macros TRVii
-void dfs(int u)
+// Undirected graph Cycle find
+bool dfs(int u, int parent) 
+{ // DFS for normal usage
+	printf(" %d", u); 
+	dfs_num[u] = DFS_BLACK; // this vertex is visited, mark it
+	//TRvii (adjlist[u], v) // try all neighbors v of vertex u
+	for (vii::iterator it = adjlist[u].begin(); it != adjlist[u].end(); it++) {
+		if (dfs_num[it->first] == DFS_WHITE) // avoid cycle
+			dfs(it->first); // v is a (neighbor, weight) pair
+		else  if (it->first != parent) return isCyclic = true;
+		else return isCyclic = false;
+	}
+	//topologicalSort.push_back(u);
+	//topologicalSort.push(u);
+	//return isCyclic;
+}
+
+// BFS
+void BFS(int s)
 {
-	cout << u;
-	dfs_num[u] = DFS_BLACK;
+	q.push(s);
+	dist[s] = 0;
 
-	TRVii(adjlist[u], v)
-		//for (vii::iterator it = adjlist[u].begin(); it != adjlist[u].end(); ++it)
-		if (dfs_num[v->first] == DFS_WHITE)
-			dfs(v->first);
+	while (!q.empty()) {
+		int u = q.front();
+		q.pop();
+		printf("Visit %d, Layer %d\n", u, dist[u]);
+
+		TRvii(adjlist[u], v)		
+			if (!dist.count(v->first)) { // if v is not visited before and reachable from u
+				dist[v->first] = dist[u] + 1;
+				q.push(v->first);	// enqueue v for next steps	
+			}
+	}
 }
 
 // Articulation point and Bridge finding
 void ArticulationPointAndBridge(int u)
 {
 	dfs_low[u] = dfs_num[u] = dfsNumberCounter++; // dfs_low[u] <= dfs_num[u]
-	TRVii(adjlist[u], v)
+	TRvii(adjlist[u], v)
 		if (dfs_num[v->first] == DFS_WHITE) {
 			dfs_parent[v->first] = u;
 			if (u == dfs_root) // if > 2 children, then articulation point
@@ -61,66 +111,104 @@ void ArticulationPointAndBridge(int u)
 			dfs_low[u] = min(dfs_low[u], dfs_num[v->first]); // update dfs_low
 }
 
-int main()
+int main() 
 {
-#pragma region Graph input
+	int numComponent = 0;
+
+#pragma region Directed graph
+	//adjlist[0].push_back(make_pair(1, 0));
+	////adjlist[1].push_back(make_pair(2, 0));
+	////adjlist[1].push_back(make_pair(0, 0));
+	//adjlist[1].push_back(make_pair(3, 0));
+	//adjlist[2].push_back(make_pair(1, 0));
+	////adjlist[2].push_back(make_pair(3, 0));
+	////adjlist[3].push_back(make_pair(1, 0));
+	//adjlist[3].push_back(make_pair(2, 0));
+	//adjlist[3].push_back(make_pair(4, 0));
+	//adjlist[4].push_back(make_pair(5, 0));
+	//adjlist[5].push_back(make_pair(7, 0));
+	//adjlist[6].push_back(make_pair(4, 0));
+	//adjlist[7].push_back(make_pair(6, 0));
+#pragma endregion
+
+#pragma region Undirected graph
+	// Undirected graph
 	adjlist[0].push_back(make_pair(1, 0));
-	//adjlist[0].push_back(make_pair(8, 0)); // new
-	adjlist[1].push_back(make_pair(0, 0));
 	adjlist[1].push_back(make_pair(2, 0));
+	adjlist[1].push_back(make_pair(0, 0));
 	adjlist[1].push_back(make_pair(3, 0));
-	//adjlist[2].push_back(make_pair(0, 0)); // new
 	adjlist[2].push_back(make_pair(1, 0));
-	adjlist[2].push_back(make_pair(3, 0));
-	adjlist[3].push_back(make_pair(4, 0));
-	adjlist[3].push_back(make_pair(1, 0));
+	adjlist[2].push_back(make_pair(3, 0));	
 	adjlist[3].push_back(make_pair(2, 0));
+	adjlist[3].push_back(make_pair(4, 0));
 	adjlist[4].push_back(make_pair(3, 0));
 	adjlist[5].push_back(make_pair(5, 0));
 	adjlist[6].push_back(make_pair(7, 0));
 	adjlist[7].push_back(make_pair(6, 0));
-	//adjlist[8].push_back(make_pair(0, 0));
 #pragma endregion
 
-#pragma region Connected Component
-	/*int numComponent = 0;
-	memset(dfs_num, false, sizeof(dfs_num));
-	Rep(i, 0, V - 1)
-	if (dfs_num[i] == false) {
-	printf("Component %d, visit:", ++numComponent);
-	dfs(i);
-	printf("\n");
-	}*/
+	// initialize the dfs_num array with -1
+	memset(dfs_num, DFS_WHITE, sizeof dfs_num);
+
+#pragma region Topological Sort
+	// Topological Sorting
+
+	//REP(i, 0, V - 1)
+	//	if (dfs_num[i] == DFS_WHITE)
+	//		dfs(i);
+	////reverse(topologicalSort.begin(), topologicalSort.end());
+	//REP(i, 0, V - 1) {
+	//	//cout << endl << topologicalSort.pop();
+	//	printf ("%d\n", topologicalSort.top());
+	//	topologicalSort.pop();
+	//}
 #pragma endregion
 
-#pragma region DFS
-	//dfs(6);
-#pragma endregion 
+	// Normal dfs
+	//dfs(3);
 
-#pragma region Articulation Point
-	dfsNumberCounter = 0;
-	memset(dfs_num, DFS_WHITE, sizeof(dfs_num));
-	printf("Bridges: \n");
-	Rep(i, 0, V - 1)
+	// Normal BFS
+
+	BFS(0);
+
+	// Directed graph Cycle find
+	/*if (dfs(0) == true)
+		cout << endl << "true";*/
+	
+	// Undirected graph Cycle find
+	// Overloaded with parent
+	/*if (dfs(5, -1)) 
+		cout << endl << "true";
+	else cout << endl << "false";*/
+
+	// Connected  component
+	/*REP (i, 0, V - 1)
 		if (dfs_num[i] == DFS_WHITE) {
-			dfs_root = i;
-			rootChildren = 0;
-			ArticulationPointAndBridge(i);
-			articulationVertex[dfs_root] = (rootChildren > 1); // special case
+			printf("Component %d, visit:", ++numComponent);
+			dfs(i);
+			printf("\n");
 		}
+	printf("There are %d connected components\n", numComponent);*/
 
-	printf("Articulation Ponints:\n");
-	Rep(i, 0, V - 1)
-		if (articulationVertex[i])
-			printf(" Vertex %d\n", i);
+	#pragma region Articulation Point
+	// Find Articulation point
+	//dfsNumberCounter = 0;
+	//memset(dfs_num, DFS_WHITE, sizeof(dfs_num));
+	//printf("Bridges: \n");
+	//REP(i, 0, V - 1)
+	//	if (dfs_num[i] == DFS_WHITE) {
+	//		dfs_root = i;
+	//		rootChildren = 0;
+	//		ArticulationPointAndBridge(i);
+	//		articulationVertex[dfs_root] = (rootChildren > 1); // special case
+	//	}
+
+	//printf("Articulation Ponints:\n");
+	//REP(i, 0, V - 1)
+	//	if (articulationVertex[i])
+	//		printf(" Vertex %d\n", i);
 #pragma endregion
 
-#pragma region printing graph
-	/*for (auto i = 0; i < V; i++)
-	for (auto iter = adjlist[i].begin(); iter != adjlist[i].end(); ++iter)
-	cout << iter->first << " " << iter->second << endl;*/
-#pragma endregion 
-
-	cin.get();
+	cin.get();	
 	return 0;
 }
